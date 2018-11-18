2be88f12e0c644c8774286693267e0922d6ed002
berrygarden <berrygarden@e-cube.tech>
2018-11-18 12:29:48 +0800

    Joomla!3.9.0Stable-Init

---
+++ administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-11-18 03:24:04 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-11-18T03:24:04+00:00	INFO 192.168.0.106	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-11-18T03:24:13+00:00	INFO 192.168.0.106	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-11-18T03:24:32+00:00	INFO 192.168.0.106	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-11-18T03:24:38+00:00	INFO 192.168.0.106	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-11-18T03:42:34+00:00	INFO 192.168.0.106	joomlafailure	Username and password do not match or you do not have an account yet.
